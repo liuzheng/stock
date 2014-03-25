c58e4dd96c91c5ee53d66f5cba95972a25606b8e
liuzheng712 <liuzheng712@gmail.com>
2014-03-25 20:33:04 +0800

    holiday

---
+++ holiday.m
@@ -0,0 +1,25 @@
+function holiday
+load data.mat
+for j = datenum('2008-01-01'):today-1
+    d=[];
+    if exist(['.\' sz{1} '\' num2str(j) '.mat'])
+       % D=dir(['.\' sz{1} '\' num2str(j) '.mat']);
+       % if D.bytes<200
+       %     d=yesterday(j,sz{1});
+       %     save(['.\' sz{1} '\' num2str(j) '.mat'],'d');
+       % end
+    else
+        d=yesterday(j,sz{1});
+        save(['.\' sz{1} '\' num2str(j) '.mat'],'d');
+        D=dir(['.\' sz{1} '\' num2str(j) '.mat']);
+    end
+end
+holiday=[];
+for i = datenum('2008-01-01'):today-1
+    a=dir(['.\' sz{1} '\' num2str(i) '.mat']);
+    if a.bytes<200
+        holiday=[holiday;i];
+    end
+end
+save('data.mat','holiday','-append');
+spk('程序运行完毕')
